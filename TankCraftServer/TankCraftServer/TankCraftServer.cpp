d9d25aac4e6d243ad6101519d2f5f8fcc3f05f28
GGN_2015 <630230141@qq.com>
2022-11-06 22:47:13 +0800

    Update TankCraftServer.cpp

--- TankCraftServer/TankCraftServer/TankCraftServer.cpp
+++ TankCraftServer/TankCraftServer/TankCraftServer.cpp
@@ -17,8 +17,8 @@ int main() {
   /* 初始化随机种子 */
   srand((unsigned int)time(NULL));
 
-  TestTcpNetManager();
-  // TestGameTcpServer();
+  // TestTcpNetManager();
+  TestGameTcpServer();
   // TestUtils();
 }
 
